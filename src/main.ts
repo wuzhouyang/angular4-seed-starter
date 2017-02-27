17a06d109d8e1a2ecf88073b58beeacf86139da6
wuzhouyang <502685630@qq.com>
2017-02-27 16:58:02 +0800

    use aot ngfactory

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 //主入口文件
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+import { platformBrowser } from '@angular/platform-browser'
 import { enableProdMode } from '@angular/core'
 
 import { AppModule } from './app/app.module'
+import { AppModuleNgFactory } from '../ngFactory/src/app/app.module.ngfactory'
 
 import './assets/styles/index.scss'
 
@@ -10,7 +12,10 @@ import './favicon.ico'
 
 if (process.env.NODE_ENV === 'prod') {
     enableProdMode()
+    platformBrowser().bootstrapModuleFactory(AppModuleNgFactory)
+} else {
+    platformBrowserDynamic().bootstrapModule(AppModule)
 }
-platformBrowserDynamic().bootstrapModule(AppModule);
+
 
 
